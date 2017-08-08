49255831c91c7b6417e5488ea023409693344926
cwonokay <cwonokay@gmail.com>
2017-08-08 16:06:25 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,45 @@
+
+
+  fetch("https://recipepuppyproxy.herokuapp.com/api/?q=bacon")
+
+    .then(
+
+   // The promise returns a response from the server.
+   function(response) {
+     // We process the response accordingly.
+     if (response.status !== 200) {
+       console.log(response.status);
+       return;
+     }
+     response.json().then(function(data) {
+       displayfunction(data);
+       console.log("Here is the data:", data.results);
+     });
+   }
+ )
+ .catch(function(err) {
+   console.log("Fetch Error :-S", err);
+ });
+
+ let display1 = document.querySelector('.container');
+
+ function displayfunction(data) {
+
+for (var i = 0; i < data.results.length; i++) {
+
+  let template = '';
+
+    template += `
+    <article>
+    <div class="Recipes">
+    <img src="${data.results[i].thumbnail}">
+    <h4>${data.results[i].title}</h4>
+ <a href="${data.results[i].href}">Link To Recipe</a>
+    </div>
+    </article>
+    `;
+    display1.innerHTML += template;
+
+}
+
+}
